90de96b813b64fae41844f0d91ebd0d9a2353081
manishkumar <manish.lalwani@embibe.com>
2024-05-04 21:41:31 +0530

    Added 2 sum solution

--- src/main/java/questions/TwoSum.java
+++ src/main/java/questions/TwoSum.java
@@ -1,7 +1,34 @@
 package questions;
 
+import java.util.*;
+
 public class TwoSum {
     public static void main(String[] args) {
 
+        int[] nums = new int[]{2, 7, 11, 15};
+
+        System.out.println(getNumswithSum(nums, 9));
+
+    }
+
+    private static Set<Integer> getNumswithSum(int[] nums,
+                                               int sum) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        HashSet<Integer> twoSumIntegers = new HashSet<>();
+
+        for (int j = 0; j < nums.length; j++) {
+
+            if (map.containsKey(sum - nums[j])) {
+                twoSumIntegers.add(nums[j]);
+                twoSumIntegers.add(sum - nums[j]);
+                return twoSumIntegers;
+
+            } else {
+                map.put(nums[j], j);
+            }
+        }
+        return Collections.EMPTY_SET;
     }
 }
