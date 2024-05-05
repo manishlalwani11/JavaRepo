622fa03d1b07cf01dcee99d369b23ae68122396a
manishkumar <manish.lalwani@embibe.com>
2024-05-05 09:54:04 +0530

    Added 3 SUM closest solution

--- src/main/java/questions/ThreeSumCloset.java
+++ src/main/java/questions/ThreeSumCloset.java
@@ -2,6 +2,34 @@ package questions;
 
 import java.util.*;
 
+/*
+Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+
+Return the sum of the three integers.
+
+You may assume that each input would have exactly one solution.
+
+
+
+Example 1:
+
+Input: nums = [-1,2,1,-4], target = 1
+Output: 2
+Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+Example 2:
+
+Input: nums = [0,0,0], target = 1
+Output: 0
+Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+
+
+Constraints:
+
+3 <= nums.length <= 500
+-1000 <= nums[i] <= 1000
+-104 <= target <= 104
+ */
+
 public class ThreeSumCloset {
     public static void main(String[] args) {
 
@@ -18,7 +46,6 @@ public class ThreeSumCloset {
             return Collections.EMPTY_LIST;
         }*/
 
-        Set<List<Integer>> threeSumIntegers = new HashSet<>();
         int i;
         int j;
         int sum;
@@ -34,7 +61,6 @@ public class ThreeSumCloset {
                 sum = nums[x] + nums[i] + nums[j];
 
                 if (sum == target) {
-                    threeSumIntegers.add(Arrays.asList(nums[x], nums[i], nums[j]));
                     // if existing diff is greater than new diff between target and sum, then replace
                     if (checkIfMapIsEmpty(target, map, sum) || checkAndReplaceInMap(target, map, sum)) {
                         map.clear();
