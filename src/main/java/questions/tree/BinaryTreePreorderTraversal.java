04fafe3e749cc54d4b0408ef4415b4807f6392e4
manishkumar <manish.lalwani@embibe.com>
2024-07-21 21:52:50 +0530

    Added BinaryTreePreorderTraversal solution

--- src/main/java/questions/tree/BinaryTreePreorderTraversal.java
+++ src/main/java/questions/tree/BinaryTreePreorderTraversal.java
@@ -1,5 +1,8 @@
 package questions.tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 https://leetcode.com/problems/binary-tree-preorder-traversal/description/
 Given the root of a binary tree, return the preorder traversal of its nodes' values.
@@ -23,7 +26,18 @@ The number of nodes in the tree is in the range [0, 100].
 Follow up: Recursive solution is trivial, could you do it iteratively?
  */
 public class BinaryTreePreorderTraversal {
-    public static void main(String[] args) {
+    List<Integer> list = new ArrayList<Integer>();
+    public List<Integer> preorderTraversal(TreeNode root) {
 
+        if(root == null) {
+            return list;
+        }
+
+        list.add(root.val);
+
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+
+        return list;
     }
 }
